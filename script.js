ca853439dce3ce6df9c00c0e70b59eb9adca627b
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2020-12-27 14:59:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,105 @@ function keyPress(e) {
   }
 }
 
+
+
+// TESTIMONIALS 
+
+(function () {
+	'use strict';
+	var slides = document.querySelectorAll('.testimonial-item'),
+		 button = document.getElementById('button'),
+		 arrows = document.querySelectorAll('.lnr'),
+		 carouselCount = 0,
+		 scrollInterval,
+		 interval = 5000;
+
+	arrows[0].addEventListener('click', function (e) {
+		e = e || window.event;
+		e.preventDefault();
+		carouselCount -= 100;
+		slider();
+		if (e.type !== 'autoClick') {
+			clearInterval(scrollInterval);
+			scrollInterval = setInterval(autoScroll, interval);
+		}
+	});
+	arrows[1].addEventListener('click', sliderEvent);
+	arrows[1].addEventListener('autoClick', sliderEvent);
+	
+	function sliderEvent(e) {
+		e = e || window.event;
+		e.preventDefault();
+		carouselCount += 100;
+		slider();
+		if (e.type !== "autoClick") {
+			clearInterval(scrollInterval);
+			scrollInterval = setInterval(autoScroll, interval);
+		}
+	}
+	
+	function slider() {
+		switch (carouselCount) {
+			case -100:
+				carouselCount = 0;
+				break;
+			case 300:
+				carouselCount = 0;
+				break;
+			default:
+				break;
+		}
+		console.log(carouselCount);
+		for (var i = 0; i < slides.length; i += 1) {
+			slides[i].setAttribute('style', 'transform:translateX(-' + carouselCount + '%)');
+		}
+	}
+	
+	// create new Event to dispatch click for auto scroll
+	var autoClick = new Event('autoClick');
+	function autoScroll() {
+		arrows[1].dispatchEvent(autoClick);
+	}
+	
+	// set timing of dispatch click events
+	scrollInterval = setInterval(autoScroll, interval);
+	
+})();
+
+
+
+
+// VIDEOS
+
+$(document).on('click','.js-videoPoster',function(ev) {
+  ev.preventDefault();
+  var $poster = $(this);
+  var $wrapper = $poster.closest('.js-videoWrapper');
+  videoPlay($wrapper);
+});
+
+
+function videoPlay($wrapper) {
+  var $iframe = $wrapper.find('.js-videoIframe');
+  var src = $iframe.data('src');
+ 
+  $wrapper.addClass('videoWrapperActive');
+  
+  $iframe.attr('src',src);
+}
+
+
+function videoStop($wrapper) {
+  
+  if (!$wrapper) {
+    var $wrapper = $('.js-videoWrapper');
+    var $iframe = $('.js-videoIframe');
+  
+  } else {
+    var $iframe = $wrapper.find('.js-videoIframe');
+  }
+  
+  $wrapper.removeClass('videoWrapperActive');
+  
+  $iframe.attr('src','');
+}
